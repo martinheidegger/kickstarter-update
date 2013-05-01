9a7b9f9bfa18f8fd7711d103e084d34f056ae696
Martin Heidegger <martin.heidegger@gmail.com>
2013-05-02 03:51:26 +0900

    * Moved from jQuery to cheerio

--- requestKickstarterData.js
+++ requestKickstarterData.js
@@ -1,17 +1,24 @@
-var $ = require('jQuery');
+var http = require('http');
+var cheerio = require('cheerio');
+
 module.exports = function(project, onComplete) {
-    $.ajax({
-        url: "http://www.kickstarter.com/projects/"+project,
-        success: function(html) {
-            var root = $(html);
-            var pledged = root.find('#pledged');
+    http.get("http://www.kickstarter.com/projects/"+project, function(res) {
+        var allData = '';
+        res.on('data', function(data) {
+            allData += data;
+        });
+        res.on('end', function() {
+            var root = cheerio.load(allData);
+            var pledged = root('#pledged');
             var pledgedData = pledged.find('data');
             onComplete(null,{
-                currency: pledgedData.data('currency'),
-                pledged: parseFloat(pledgedData.data('value')),
-                goal: parseFloat(pledged.data('goal')),
-                end: new Date(root.find('h5.ksr_page_timer').data('end_time'))
+                currency: pledgedData.attr('data-currency'),
+                pledged: parseFloat(pledgedData.attr('data-value')),
+                goal: parseFloat(pledged.attr('data-goal')),
+                end: new Date(root('h5.ksr_page_timer').attr('data-end_time'))
             })
-        }
+        });
+    }).on('error', function() {
+        onComplete(err, null);
     });
 };
